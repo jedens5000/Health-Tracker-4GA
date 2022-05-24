c773c70c6e14851a52037cc8228c946f17e1daf5
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-05-24 00:59:57 +0000

    login card created

--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -7,35 +7,26 @@ export const Demo = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
+		<div className="container" style={{ marginTop:"5%", display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+			<div className="card text-bg-primary mb-3" style={{maxWidth: "18rem"}}>
+				<div className="card-header" style = {{display: "flex", justifyContent: "center"}}>Login</div>
+				<div className="card-body" style = {{display: "flex", flexDirection: "column", textAlign: "center" }}>
+					<h1 style = {{fontSize:"25px", fontFamily:"Raleway"}}>Email</h1>
+					<input types="email" className="form-control" placeholder="Enter your email address"></input>
+						<br></br>
+					<h1 style = {{fontSize:"25px", fontFamily:"Raleway"}}>Password</h1>
+					<input types="email" className="form-control" placeholder="Enter your email address"></input>
+						<br></br>
+					<div className = "forgotPasswordBtn">
+						<button className="forgotPassword">ForgotPassword</button>
+					</div>
+					<div className = "signUpBtn">
+						<button className="signUp">Sign Up</button>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	);
 };
+
