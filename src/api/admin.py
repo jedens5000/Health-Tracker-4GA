b14bcc591046adfac37ba19bea59836bc04da62f
jedens5000 <mrjonedens@gmail.com>
2022-06-01 23:56:03 +0000

    UserAcct & Issues tables added

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,8 +1,9 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, UserAcct, Issues
 from flask_admin.contrib.sqla import ModelView
+
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
@@ -11,7 +12,8 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(UserAcct, db.session))
+    admin.add_view(ModelView(Issues, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -2,18 +2,60 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class UserAcct(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    pw = db.Column(db.String(100), unique=True, nullable=False)
+    first_name = db.Column(db.String(100), unique=False, nullable=False)
+    issues = db.relationship('Issues', backref='useracct', lazy=True)
+
+
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return self.first_name           
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "first_name": self.first_name,            
         }
+
+class Issues(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    issue1 = db.Column(db.String(100), unique=False, nullable=False)
+    issue2 = db.Column(db.String(100), unique=False, nullable=True)
+    issue3 = db.Column(db.String(100), unique=False, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('useracct.id'), nullable= False)
+
+
+    def __repr__(self):
+        return self.issue1
+           
+    def serialize(self):
+        return {
+            "id": self.id,
+            "issue1": self.issue1,       
+            "issue2": self.issue2,       
+            "issue3": self.issue3,   
+        }
+
+
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     # is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     def __repr__(self):
+#         return f'<User {self.email}>'
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "email": self.email,
+#             # do not serialize the password, its a security breach
+#         }
