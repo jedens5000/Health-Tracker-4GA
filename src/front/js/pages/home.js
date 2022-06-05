6cc9f644b0b027fc761fb4e57784d61b5ebe536b
jedens5000 <mrjonedens@gmail.com>
2022-06-05 23:52:57 +0000

    updated login logo classname to apply existing css

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,24 +4,21 @@ import mindsetLogo from "../../img/mindset-logo.png";
 import "../../styles/home.css";
 import { Navbar } from "../component/navbar";
 
-
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div id="homePhoto" >
+    <div id="homePhoto">
       <div className="position-absolute top-50 start-50 translate-middle">
         <div className="d-flex flex-column align-items-center">
           <img src={mindsetLogo} />
-          <div className="card">
-            <div id="homeCardBody">
-              <p>MindSet - /ˈmaɪn(d)ˌset/ - noun</p>
-              <p>: a mental attitude or inclination</p>
-              <p>: a fixed state of mind</p>
-              <a className="btn btn-primary" href="./login" role="button">
-                Get started now
-              </a>
-            </div>
+          <div id="homeCardBody">
+            <p>MindSet - /ˈmaɪn(d)ˌset/ - noun</p>
+            <p>: a mental attitude or inclination</p>
+            <p>: a fixed state of mind</p>
+            <a className="btn btn-primary" href="./login" role="button">
+              Get started now
+            </a>
           </div>
         </div>
       </div>
