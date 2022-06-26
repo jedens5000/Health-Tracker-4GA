f597974d0598ac1f6eb95e3f6f5cf216c1402b88
Chadmichaelg <chadmgrubbs@gmail.com>
2022-06-26 16:12:57 +0000

    text notification update

--- src/api/MedTextReminders.py
+++ src/api/MedTextReminders.py
@@ -1,15 +1,7 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from twilio.rest import Client
 # from twilio_credentials import cellphone, twilio_account, twilio_token, twilio_number
-
-# medication_reminders = [
-#     "Time to take your medication",
-#     "Did you take your medication yet?",
-#     "Don't forget to take your medication]
-
-
-
 
 
 def send_message(notification):
@@ -22,27 +14,23 @@ def send_message(notification):
 
     # client = Client("AC8b23dea2a92c7b85b3e6aacf1c27de02", "697f3695df9e93e49f3332e3fd1014e6")
 
-    message = client.messages \
-    .create(
-         messaging_service_sid='MGa0cedfa2c6b81978eed881985cd0e9ca',
-         body='Time to take your medication',
-         send_at=datetime(2022, 26, 6, 12, 11, 00),
-         schedule_type='fixed',
-         status_callback='https://webhook.site/xxxxx',
-         to='+19546508821'
-     )
+    # schedule message to be sent 61 minutes after current time
+send_when = datetime.utcnow() + timedelta(minutes=61)
+
+# send the SMS
+messaging_service_sid = os.environ['TWILIO_MESSAGING_SERVICE_SID']
+message = client.messages.create(
+    from_=messaging_service_sid,
+    to='+19546508821',  # ← your phone number here
+    body='Friendly reminder that it is time to take your medication.',
+    schedule_type='fixed',
+    send_at=send_when.isoformat() + 'Z',
+)
 
 print(message.sid)
 
-    # message = client.messages \
-    # .create(
-    #     to=+19546508821, 
-    #     from_=+19705125409, 
-    #     body=notification)
-
-    # notification = medication_reminders [random.randint(0, len(medication_reminders))]
+ 
 
 
-
-for item in message:
-    send_message(item)
+# for item in message:
+#     send_message(item)
