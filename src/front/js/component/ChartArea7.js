55cf9c6133b2e837aef357bcf2ec9685e30de3c7
Jon Edens <mrjonedens@gmail.com>
2022-07-09 23:53:44 +0000

    Working Pie chart added

--- src/front/js/component/ChartArea7.js
+++ src/front/js/component/ChartArea7.js
@@ -43,8 +43,8 @@ export const ChartArea7 = () => {
   // const testArray = [...dataArray[0], ...dataArray[1], ...dataArray[2]];
   // console.log(testArray);
   const issue1 = dataArray[0][0].issue;
-  const issue2 = dataArray[1][0].issue;
-  const issue3 = dataArray[2][0].issue;
+  const issue2 = dataArray[2][0].issue;
+  const issue3 = dataArray[4][0].issue;
   console.log(issue1, issue2, issue3);
 
   return (
@@ -54,7 +54,7 @@ export const ChartArea7 = () => {
         <ResponsiveContainer width="100%" height={300}>
           <AreaChart
             width={800}
-            height={400}
+            height={300}
             data={dataArray[0]}
             margin={{
               top: 10,
@@ -82,8 +82,8 @@ export const ChartArea7 = () => {
         <ResponsiveContainer width="100%" height={300}>
           <AreaChart
             width={800}
-            height={400}
-            data={dataArray[1]}
+            height={300}
+            data={dataArray[2]}
             margin={{
               top: 10,
               right: 30,
@@ -110,8 +110,8 @@ export const ChartArea7 = () => {
         <ResponsiveContainer width="100%" height={300}>
           <AreaChart
             width={800}
-            height={400}
-            data={dataArray[2]}
+            height={300}
+            data={dataArray[4]}
             margin={{
               top: 10,
               right: 30,
@@ -133,48 +133,6 @@ export const ChartArea7 = () => {
           </AreaChart>
         </ResponsiveContainer>
       </div>
-      {/* <div className="chart-wrapper">
-        <p className="text-center chart-name">{issue2}</p>
-        <LineChart
-          width={800}
-          height={300}
-          data={dataArray[1]}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-          className={"chart-bg"}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Line type="monotone" dataKey="value" stroke="#8884d8" />
-        </LineChart>
-      </div>
-      <div className="chart-wrapper">
-        <p className="text-center chart-name">{issue3}</p>
-        <LineChart
-          width={800}
-          height={300}
-          data={dataArray[2]}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-          className={"chart-bg"}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Line type="monotone" dataKey="value" stroke="#8884d8" />
-        </LineChart>
-      </div> */}
     </div>
   );
 };
--- src/front/js/component/ChartLine7.js
+++ src/front/js/component/ChartLine7.js
@@ -43,8 +43,8 @@ export const ChartLine7 = () => {
   // const testArray = [...dataArray[0], ...dataArray[1], ...dataArray[2]];
   // console.log(testArray);
   const issue1 = dataArray[0][0].issue;
-  const issue2 = dataArray[1][0].issue;
-  const issue3 = dataArray[2][0].issue;
+  const issue2 = dataArray[2][0].issue;
+  const issue3 = dataArray[4][0].issue;
   console.log(issue1, issue2, issue3);
 
   return (
@@ -74,45 +74,49 @@ export const ChartLine7 = () => {
       </div>
       <div className="chart-wrapper">
         <p className="text-center chart-name">{issue2}</p>
-        <LineChart
-          width={800}
-          height={300}
-          data={dataArray[1]}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-          className={"chart-bg"}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Line type="monotone" dataKey="value" stroke="#8884d8" />
-        </LineChart>
+        <ResponsiveContainer width="100%" height={300}>
+          <LineChart
+            width={800}
+            height={300}
+            data={dataArray[2]}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+            className={"chart-bg"}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Line type="monotone" dataKey="value" stroke="#8884d8" />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
       <div className="chart-wrapper">
         <p className="text-center chart-name">{issue3}</p>
-        <LineChart
-          width={800}
-          height={300}
-          data={dataArray[2]}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-          className={"chart-bg"}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Line type="monotone" dataKey="value" stroke="#8884d8" />
-        </LineChart>
+        <ResponsiveContainer width="100%" height={300}>
+          <LineChart
+            width={800}
+            height={300}
+            data={dataArray[4]}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+            className={"chart-bg"}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Line type="monotone" dataKey="value" stroke="#8884d8" />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
