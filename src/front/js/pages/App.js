df0a992df0fa56f724e7afbff075d5b4bc7227a3
Chadmichaelg <chadmgrubbs@gmail.com>
2022-07-15 13:24:04 +0000

    style page

--- src/front/js/pages/App.js
+++ src/front/js/pages/App.js
@@ -4,16 +4,32 @@ import React from "react"
 // import PickDateOfBirth from "../component/PickDateOfBirth";
 // import BookDrivingSlot from "../component/BookDrivingSlot";
 import RoomScheduler from "../component/SMSForm";
+import chartBamboo from "../../img/bamboo8.jpg";
+import { NavMenu } from "../component/navbar";
+
+
 function App() {
   return (
+    <div
+      style={{
+        backgroundImage: `url(${chartBamboo})`,
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        backgroundRepeat: "no-repeat",
+        height: "200vh",
+      }}
+    >
+      <NavMenu />
+
     <div className="App">
       {/* <PickDateOfBirth /> */}
       {/* <hr className="k-my-8" />
       <BookDrivingSlot /> */}
-      <hr className="k-my-8" />
+      {/* <hr className="k-my-8" /> */}
       <RoomScheduler />
     </div>
   );
-}
+</div>
+)}
 
 export default App;
